da6cb31cb3fec752f1cc66d608ad5e01281ef7f5
Kamil Nowak <kamilnowakx98@gmail.com>
2020-02-06 01:30:42 +0100

    caught canvas context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Canvas from "./components/Canvas";
 function App() {
   return (
     <div className="App">
-
+      <Canvas/>
     </div>
   );
 }
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import 
+    React,
+    {useRef,
+    useEffect} from 'react';
 
 const Canvas = () =>{
-    return (
-    <div>
-        <canvas ref="canvas"/>
-    </div>
-        )
-}
+    const ref = useRef();
 
+    useEffect(()=>{
+        const canvas = ref.current;
+        const context = canvas.getContext('2d');
+    })
+    return (
+        <canvas
+            ref={ref}
+            style={{width: '100px', height: '100px' }}
+        />
+    );
+}
 
 export default Canvas;
