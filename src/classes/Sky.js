50000205276548a934dbd3f429338d406687ae64
Kamil Nowak <kamilnowakx98@gmail.com>
2020-02-07 00:04:37 +0100

    draw contellation

--- src/classes/Sky.js
+++ src/classes/Sky.js
@@ -4,7 +4,7 @@ class Sky{
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
         this.backgroundColor = backgroundColor;
-        this.starsManager = new StarsManager(this.context,StarsManager.prepareStarsData(numberOfStar));
+        this.starsManager = new StarsManager(this.context,StarsManager.prepareStarsData(numberOfStar),30,2);
     }
     initCanvas(){
         const width = window.innerWidth;
@@ -18,12 +18,24 @@ class Sky{
     clearCanvas(){
         const width =  window.innerWidth;
         const height = window.outerHeight;
-        this.context.fillStyle = "#000";
+        this.context.fillStyle = this.backgroundColor;
+        this.context.fillRect(0,0,width,height);
+    }
+    drawOverlayer(){
+        const width =  window.innerWidth;
+        const height = window.outerHeight;
+        const gradient = this.context.createRadialGradient(width / 2, height / 2, 250,width / 2, height / 2, width / 2);
+        gradient.addColorStop(0,"rgba(0,0,0,0)");
+        gradient.addColorStop(1,"rgba(0,0,0,0.8)");
+
+        this.context.fillStyle = gradient;
         this.context.fillRect(0,0,width,height);
     }
     draw(){
         this.clearCanvas();
         this.starsManager.move();
+        this.starsManager.drawConstellation();
+        this.drawOverlayer();
         window.requestAnimationFrame(() => this.draw());
         
 
--- src/classes/StarsManager.js
+++ src/classes/StarsManager.js
@@ -1,12 +1,15 @@
 import Star from "./Star";
 
 class StarsManager{
-    constructor(context,stars){
+    constructor(context,stars,maxStarsInConstellation = 10, minStarsInConstellation = 3){
         this.context = context;
+        this.starsInConstellation = maxStarsInConstellation - minStarsInConstellation;
+        this.minStarsInConstellation = minStarsInConstellation;
         this.stars = stars.map(({x,y,radius,color,speed})=>{
             const star = new Star(context,x,y,color,radius,speed);
             return star;
         })
+        this.generatorConstellation();
     }
 
     draw(){
@@ -14,12 +17,52 @@ class StarsManager{
             star.draw();
         });
     }
+
     move(){
-        console.log('move');
         this.stars.forEach((star) => {
             star.move();
         });
     }
+
+    drawConstellation(){
+        const {stars} = this.constellation;
+
+        if(stars.lenght < 2){
+            return;
+        }
+        this.context.beginPath();
+
+        stars.forEach(({x,y},index) => {
+            if(index === 0){
+                this.context.moveTo(x, y)
+            }else{
+                this.context.lineTo(x,y);
+            }
+        })
+        this.context.strokeStyle = "#ff9933";
+        this.context.stroke();
+
+    }
+
+    generatorConstellation(){
+        const width =  window.innerWidth;
+        const height = window.outerHeight;
+
+        const x = width / 2 * (Math.random() + 0.3);
+        const y = height / 2 * (Math.random( + 0.3));
+        const radius = (height /6 + width / 10) * Math.random()+66;
+
+        this.constellation = {
+            stars: this.stars.filter(star => (
+                star.x > x - radius
+                && star.x < x + radius
+                && star.y > y - radius
+                && star.y < y + radius
+            )).slice(0, Math.floor(Math.random()*this.starsInConstellation)+this.minStarsInConstellation)
+        }
+
+    }
+
     static prepareStarsData(count){
         const stars = [];
         const width =  window.innerWidth;
@@ -38,7 +81,6 @@ class StarsManager{
                 }
             })
         }
-        console.log(stars);
         return stars;
     }
 }
